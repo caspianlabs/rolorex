cc67a125b303eb63bc487f85967d01c900448aa1
Lev Lazinskiy <lev@levlaz.org>
2020-04-21 18:06:19 -0700

    ui: Implement base template for public (#6)
    This commit adds a basic base template for the public site.
    
    fixes #5

--- public/tests.py
+++ public/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class PublicIndexViewTests(TestCase):
+    def test_index_page_loads(self):
+        """Going to the index page shows me log in info."""
+        response = self.client.get(reverse('public:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Welcome to Rolorex")
+
