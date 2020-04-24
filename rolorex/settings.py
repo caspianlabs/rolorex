27ebe9f1db10e235be73517e8cce302ea43b84a2
Lev Lazinskiy <lev@levlaz.org>
2020-04-23 17:14:56 -0700

    ui: Implement basic theme
    This commit adds a basic them for the index, register, and login pages
    using Bootstrap.

--- rolorex/settings.py
+++ rolorex/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rolorex.apps.RolorexAdminConfig',
+    'widget_tweaks',
     'app.apps.AppConfig',
     'public.apps.PublicConfig',
     'registration.apps.AccountConfig'
